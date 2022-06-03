903dce1bd1ff4695946748973f777d8f4a47f159
rimirim <rimizzaang@naver.com>
2022-06-03 20:58:55 +0900

    [유아림] feat : 자잘한 수정 [MRS]

--- tissue_back/src/main/java/com/example/Tissue_back/controller/MemberController.java
+++ tissue_back/src/main/java/com/example/Tissue_back/controller/MemberController.java
@@ -28,8 +28,8 @@ public class MemberController {
     }
 
     // 아이디 중복 여부 체크
-    @GetMapping("/checkId/{id}")
-    public Boolean checkId (@PathVariable("id") String memberId){
+    @GetMapping("/checkId/{memberId}")
+    public Boolean checkId (@PathVariable("memberId") String memberId){
         log.info("checkId(): " + memberId);
 
         Boolean checkId = service.checkId(memberId);
