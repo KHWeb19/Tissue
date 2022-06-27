e5534e92e6dd94db043ec65deb9db2dfd3e951c2
kangleejeong <krj1226@naver.com>
2022-06-27 19:23:26 +0900

    [강이정] 고객센터 시스템 (QnA) [CSP] 자주묻는질문 (수정페이지)

---
+++ tissue_front/src/views/qna/QnaBestModifyPage.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <qna-best-modify v-if="qnaBest" :qnaBest="qnaBest" @submit="onSubmit"/>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { mapActions, mapState } from 'vuex'
+import QnaBestModify from '@/components/qna/QnaBestModify.vue'
+export default {
+  name: 'QnaBestModifyPage',
+  components: {
+    QnaBestModify
+  },
+  props: {
+    qnaBestNo: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    ...mapState(['qnaBest'])
+  },
+  methods: {
+    ...mapActions(['fetchQnaBest']),
+    onSubmit (payload) {
+      const { qnaBestCategory, qnaBestTitle, qnaBestContent } = payload
+      axios.put(`qnaBest/${this.qnaBestNo}`, { qnaBestCategory, qnaBestTitle, qnaBestContent })
+        .then(() => {
+          alert('자주묻는 질문이 수정되었습니다.')
+          this.$router.replace({
+            name: 'QnaListPage'
+          })
+        })
+        .catch(() => {
+          alert('수정 실패...에러 해결하세여...')
+        })
+    }
+  },
+  created () {
+    this.fetchQnaBest(this.qnaBestNo)
+      .catch(() => {
+        alert('게시물 DB 조회 실패')
+        this.$router.back()
+      })
+  }
+}
+</script>
