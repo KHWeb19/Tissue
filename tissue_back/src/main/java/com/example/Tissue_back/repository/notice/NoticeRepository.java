224ee644333ca83db76778dc4e7a74361282faa4
rimirim <96419126+rimirim@users.noreply.github.com>
2022-06-28 21:34:51 +0900

    [유아림] 공지사항 검색 기능 구현 [SCHS]

--- tissue_back/src/main/java/com/example/Tissue_back/repository/notice/NoticeRepository.java
+++ tissue_back/src/main/java/com/example/Tissue_back/repository/notice/NoticeRepository.java
@@ -1,11 +1,13 @@
 package com.example.Tissue_back.repository.notice;
 
+import com.example.Tissue_back.entity.Performance;
 import com.example.Tissue_back.entity.notice.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
@@ -13,4 +15,6 @@ public interface NoticeRepository extends JpaRepository<Notice, Long> {
     @Modifying
     @Query("update Notice n set n.noticeView = n.noticeView + 1 where n.noticeNo = :noticeNo")
     int updateNoticeView(Long noticeNo);
+
+    List<Notice> findByNoticeTitleContaining (String keyword);
 }
