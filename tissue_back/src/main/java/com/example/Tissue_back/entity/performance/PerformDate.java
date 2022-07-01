368cdf740f5ed48f57c7b9e6a63f96c3906ad31a
SeohyunNoh <sarahnoh7@naver.com>
2022-07-01 22:14:49 +0900

    [노서현] 공연 단일 날짜 엔티티 등록

--- tissue_back/src/main/java/com/example/Tissue_back/entity/performance/PerformDate.java
+++ tissue_back/src/main/java/com/example/Tissue_back/entity/performance/PerformDate.java
@@ -1,4 +1,25 @@
 package com.example.Tissue_back.entity.performance;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class PerformDate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="perform_date_no")
+    private Long performDateNo;
+
+    private Long performNo;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate performDate;
 }
