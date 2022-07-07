d3fc8ea3e752155e93aea55daf169beade7a54a8
kangleejeong <krj1226@naver.com>
2022-07-07 16:21:19 +0900

    [강이정] 랭킹 시스템 [RP] : test

---
+++ tissue_front/src/components/ranking/RankingList.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-form @submit.prevent="onSubmit">
+        <v-date-picker v-model="bookDate"/>
+        <v-btn type="submit"/>
+    </v-form>
+</template>
+
+<script>
+export default {
+  name: 'RankingList',
+  data () {
+    return {
+      bookDate: ''
+    }
+  },
+  methods: {
+    onSubmit () {
+      const { bookDate } = this
+      console.log(bookDate)
+      this.$emit('submit', {bookDate})
+    }
+  }
+}
+</script>
---
+++ tissue_front/src/views/ranking/RankingPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ranking-list @submit="onSubmit"/>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import RankingList from '@/components/ranking/RankingList.vue'
+
+export default {
+    name: 'RankingPage',
+    components: {
+        RankingList
+    },
+    onSubmit (payload) {
+        const { bookDate } = payload
+        axios.get('ranking/list', { params: { bookDate }})
+          .then(() => {
+              alert('success')
+          })
+          .catch(() => {
+              alert('fail')
+          })
+    }
+}
+</script>
